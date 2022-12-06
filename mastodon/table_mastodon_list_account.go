fa4954c4d43491bdad75f561669a2df908ce5d1c
Jon Udell <judell@turbot.com>
2022-12-05 19:05:29 -0800

    Revert "try a friendlier message for missing qual"
    This reverts commit 21121a0709e72cad8ce7970b96cb98b739e9e0f4.

--- mastodon/table_mastodon_list_account.go
+++ mastodon/table_mastodon_list_account.go
@@ -2,7 +2,9 @@ package mastodon
 
 import (
 	"context"
+	//"encoding/json"
 	"fmt"
+	//"net/http"
 
 	"github.com/mattn/go-mastodon"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
@@ -13,7 +15,7 @@ func tableMastodonListAccount() *plugin.Table {
 		Name: "mastodon_list_account",
 		List: &plugin.ListConfig{
 			Hydrate:    listListAccount,
-			KeyColumns: plugin.OptionalColumns([]string{"list_id"}),
+			KeyColumns: plugin.SingleColumn("list_id"),
 		},
 		Columns: accountColumns(),
 	}
@@ -26,17 +28,7 @@ func listListAccount(ctx context.Context, d *plugin.QueryData, h *plugin.Hydrate
 		return nil, fmt.Errorf("unable to establish a connection: %v", err)
 	}
 
-	plugin.Logger(ctx).Debug("listListAccount", "keycols", d.Quals)
-
-	quals := d.Table.Get.KeyColumns
-	list_id := quals.Find("list_id").String()
-
-	if list_id == "" {
-		plugin.Logger(ctx).Debug("listListAccount: list_id is empty")
-		account := mastodon.Account{ID: "`list_id` is required, please provide it in a `where` or `join on` clause"}
-		d.StreamListItem(ctx, &account)
-		return nil, nil
-	}
+	list_id := d.KeyColumnQuals["list_id"].GetStringValue()
 
 	accounts, err := client.GetListAccounts(ctx, mastodon.ID(list_id))
 	if err != nil {
