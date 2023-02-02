661aebc84d694a9ccf1d2e8658de161fae2e0018
Jon Udell <jon@jonudell.info>
2023-02-01 21:01:05 -0800

    add optional key column server to mastodon_weekly_activity

--- mastodon/table_mastodon_weekly_activity.go
+++ mastodon/table_mastodon_weekly_activity.go
@@ -2,10 +2,12 @@ package mastodon
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strconv"
 	"time"
 
-	"github.com/mattn/go-mastodon"
 	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v4/plugin/transform"
@@ -16,13 +18,32 @@ func tableMastodonWeeklyActivity() *plugin.Table {
 		Name: "mastodon_weekly_activity",
 		List: &plugin.ListConfig{
 			Hydrate: listWeeklyActivity,
+			KeyColumns: []*plugin.KeyColumn{
+				{
+					Name:    "server",
+					Require: plugin.Optional,
+				},
+			},
 		},
 		Columns: weeklyActivityColumns(),
 	}
 }
 
+type mastodonWeeklyActivity struct {
+	Server        string `json:"server"`
+	Week          string `json:"week"`
+	Statuses      string `json:"statuses"`
+	Logins        string `json:"logins"`
+	Registrations string `json:"registrations"`
+}
+
 func weeklyActivityColumns() []*plugin.Column {
 	return []*plugin.Column{
+		{
+			Name:        "server",
+			Type:        proto.ColumnType_STRING,
+			Description: "Server whose activity is reported.",
+		},
 		{
 			Name:        "week",
 			Type:        proto.ColumnType_TIMESTAMP,
@@ -48,24 +69,40 @@ func weeklyActivityColumns() []*plugin.Column {
 }
 
 func listWeeklyActivity(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	client, err := connect(ctx, d)
-	if err != nil {
-		return nil, fmt.Errorf("unable to establish a connection: %v", err)
+	config := GetConfig(d.Connection)
+	server := *config.Server
+	quals := d.KeyColumnQuals
+	if quals["server"] != nil {
+		server = quals["server"].GetStringValue()
 	}
-
-	instanceActivity, err := client.GetInstanceActivity(ctx)
+	client := &http.Client{}
+	url := fmt.Sprintf("%s/api/v1/instance/activity", server)
+	plugin.Logger(ctx).Debug("listWeeklyActivity", "url", url)
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := client.Do(req)
+	var activities []mastodonWeeklyActivity
+	defer res.Body.Close()
+	decoder := json.NewDecoder(res.Body)
+	err := decoder.Decode(&activities)
 	if err != nil {
-		return nil, err
+		plugin.Logger(ctx).Error(err.Error())
 	}
-
-	for _, activity := range instanceActivity {
-		d.StreamListItem(ctx, activity)
+	for _, activity := range activities {
+		a := mastodonWeeklyActivity{
+			Server:        server,
+			Week:          activity.Week,
+			Statuses:      activity.Statuses,
+			Logins:        activity.Logins,
+			Registrations: activity.Registrations,
+		}
+		d.StreamListItem(ctx, a)
 	}
 
 	return nil, nil
 }
 
 func week(ctx context.Context, input *transform.TransformData) (interface{}, error) {
-	week := input.Value.(mastodon.Unixtime)
-	return time.Time(week), nil
+	weekAsStr := input.Value.(string)
+	week, _ := strconv.ParseInt(weekAsStr, 10, 64)
+	return time.Unix(week, 0), nil
 }
