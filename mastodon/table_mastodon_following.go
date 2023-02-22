242cfcf23dcb2d1f142ef7ac15f7dacbd1db98ff
luis cavalcante <luis@turbot.com>
2023-02-22 00:46:14 -0300

    WIP: handling pagination for followers and following

--- mastodon/table_mastodon_following.go
+++ mastodon/table_mastodon_following.go
@@ -17,5 +17,32 @@ func tableMastodonFollowing() *plugin.Table {
 }
 
 func listFollowing(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	return listFollows(ctx, "following", d, h)
+	logger := plugin.Logger(ctx)
+
+	client, err := connect(ctx, d)
+	if err != nil {
+		logger.Error("mastodon_rule.listMastodonRule", "connect_error", err)
+		return nil, err
+	}
+
+	accountCurrentUser, err := client.GetAccountCurrentUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// apiMaxPerPage := 40
+	// pg := mastodon.Pagination{Limit: int64(apiMaxPerPage)}
+	// pg := mastodon.Pagination{}
+
+	// rules, err := client.GetAccountFollowing(ctx, accountCurrentUser.ID, &pg)
+	rules, err := client.GetAccountFollowing(ctx, accountCurrentUser.ID, nil)
+	if err != nil {
+		logger.Error("mastodon_rule.listMastodonRule", "query_error", err)
+		return nil, err
+	}
+	for _, rule := range rules {
+		d.StreamListItem(ctx, rule)
+	}
+
+	return nil, nil
 }
